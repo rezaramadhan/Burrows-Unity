615fb99a707266ceeb96bf5f7338fcd2a4ce8539
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2017-03-13 17:53:42 +0700

    Good & bad items is now fruits and stuff

--- Burrows/Assets/Scripts/SpawnObjects.cs
+++ Burrows/Assets/Scripts/SpawnObjects.cs
@@ -6,18 +6,17 @@ using UnityEngine.UI;
 public class SpawnObjects : MonoBehaviour {
 	public GameObject good;
 	public GameObject bad;
+	public GameObject[] items;
 	private StateController state;
 	public float zpos;
 	public float spawnrate;
 	private int i;
+	private int idx;
 
 	public Text result;
 	// Use this for initialization
 	void Start () {
 		i = 0;
-		//Instantiate (good, new Vector3(0,0,zpos), Quaternion.identity);
-		//Instantiate (bad, new Vector3(-3,0,zpos), Quaternion.identity);
-		//Instantiate (good, new Vector3(3,0,zpos), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
@@ -34,14 +33,18 @@ public class SpawnObjects : MonoBehaviour {
 		GameObject spawn;
 
 		while (!state.end) {
-			for (int i = -3; i <= 3; i = i + 3) {
-				rand = (int)Random.Range (1, 100);
-				if (rand % 2 == 1)
-					spawn = good;
-				else
-					spawn = bad;
-				Instantiate (spawn, new Vector3 (i, 0, zpos), Quaternion.identity);
-			}
+			i = ((int)Random.Range (1, 3));
+			idx = (int)Random.Range (0, items.Length);
+			Instantiate (items[idx], new Vector3 ((i - 1) * 3, 0, zpos), Quaternion.identity);
+//
+//			for (int i = -3; i <= 3; i = i + 3) {
+//				rand = (int)Random.Range (1, 100);
+//				if (rand % 2 == 1)
+//					spawn = good;
+//				else
+//					spawn = bad;
+//				Instantiate (spawn, new Vector3 (i, 0, zpos), Quaternion.identity);
+//			}
 			yield return new WaitForSeconds (delay);
 		}
 	}
--- Burrows/Assets/Scripts/TestBehavior.cs
+++ Burrows/Assets/Scripts/TestBehavior.cs
@@ -6,15 +6,23 @@ public class TestBehavior : MonoBehaviour {
 	public Rigidbody rb;
 	public float x;
 	public float y;
+	private Vector3 rotationVector;
+	private float maxRotation = 216f;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		float vx = Random.Range (-1 * maxRotation, maxRotation);
+		float vy = Random.Range (-1 * maxRotation, maxRotation);
+		float vz = Random.Range (-1 * maxRotation, maxRotation);
+		rotationVector = new Vector3(vx, vy, vz);
+		Debug.Log (rotationVector);
 		rb.AddForce (new Vector3 (0, y, x),ForceMode.Impulse);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Rotate (rotationVector * Time.deltaTime);
 	}
 
 	void OnBecameInvisible () {
